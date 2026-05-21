9d08fbd68e7c3ae41cc744e3754b276aaa2fa9bf
DESKTOP-R889J86\yangrui <youngray9706@gmail.com>
2026-05-21 22:28:12 +0800

    解决bug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+/*
+ * @Author: YangRui
+ * @Date: 2026-05-21 21:47:42
+ * @LastEditors: YangRui
+ * @LastEditTime: 2026-05-21 22:25:56
+ * @Description: 请输入
+ */
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './'
 })
